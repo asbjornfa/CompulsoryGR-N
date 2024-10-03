f6a3ff74f9d977c53870d79adac6ccd7bb16c737
asbjornfa <asbjornfa@gmail.com>
2024-10-03 09:50:00 +0200

    paperAtom and shop

--- client/src/atoms/PaperAtom.tsx
+++ client/src/atoms/PaperAtom.tsx
@@ -12,9 +12,3 @@ export const CreateNewPaperAtom = atom<RequestCreatePaperDTO>({
     price: 0,
     discontinued: false
 })
-
-export const searchAtom = atom<string>(""); // For search text
-export const minPriceAtom = atom<number | undefined>(undefined); // For minimum price
-export const maxPriceAtom = atom<number | undefined>(undefined); // For maximum price
-export const sortByAtom = atom<string>(""); // For sorting field (name, price, stock)
-export const sortOrderAtom = atom<string>("asc"); // For sort order (ascending or descending)
